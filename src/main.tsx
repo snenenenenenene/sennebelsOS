0fb5a8385835f96f0851a045dfc9ad4f5b33e08d
senne <sennebels@gmail.com>
2023-08-16 11:00:10 +0200

    feat: :sparkles: analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
@@ -5,6 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Analytics />
     <App />
   </React.StrictMode>
 );
